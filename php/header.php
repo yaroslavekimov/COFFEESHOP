28b24472502b20ce9bb1b08f1d421d149250712b
yaroslavekimov <yaroslavmur@mail.ru>
2022-02-22 22:19:39 +0300

    header

---
+++ php/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Coffee-shop</title>
+	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <header>
+        <div class="header">
+            <div class="headerLogo">
+                <a href="/coffee-shop"><img src="img/logo.png" alt="coffee-shop"></a>
+            </div>
+            <div class="headerMenu">
+                <ul class="headerList">
+                    <li class="headerItem"><a href="#">ГЛАВНАЯ</a></li>
+                    <li class="headerItem"><a href="#">ТОВАРЫ</a></li>
+                    <li class="headerItem"><a href="#">О НАС</a></li>
+                    <li class="headerItem"><a href="#">КОНТАКТЫ</a></li>
+                </ul>
+            </div>
+        </div>
+    </header>
--- php/views/template_view.php
+++ php/views/template_view.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-	<meta charset="utf-8">
-	<title>Coffee-shop</title>
-	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-	<?php include 'php/views/'.$content_view; ?>
+<?php 
+	include 'php/header.php';
+	include 'php/views/'.$content_view;
+?>
 </body>
 </html>
