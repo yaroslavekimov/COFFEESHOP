f63b98324614d3c5145dc1576ac97d2bbf6369f6
yaroslavekimov <yaroslavmur@mail.ru>
2022-02-22 21:05:24 +0300

    fonts+libs+hello world

--- php/views/general_view.php
+++ php/views/general_view.php
@@ -1,5 +1,7 @@
-<h1>Добро пожаловать на наш первый сайт!</h1>
-<table>
-	<tr><th>ID</th><th>Title</th><th>Content</th><th>Postcard</th></tr>
-	
-</table>
+<main>
+	<section class="box">
+		<?php
+			echo 'Hello world';
+		?>
+	</section>
+</main>
