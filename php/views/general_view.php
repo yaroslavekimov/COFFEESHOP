5009efca24f905c93a0f937f4094d855c2340d16
yaroslavekimov <yaroslavmur@mail.ru>
2022-02-23 22:54:21 +0300

    generalPage

--- php/views/general_view.php
+++ php/views/general_view.php
@@ -1,1 +1,8 @@
 	
+	<div class="generalPage">
+		<div class="generalPageImg"><img src="products/canephora.png" alt="coffee shop"></div>
+		<div class="generalPagePromo">
+			<h1 class="generalPageTitle"><span>Пора сделать</span> <br> &nbsp; Перерыв на Кофе!</h1>
+			<button class="generalPageButton"><a href="#">Shop now</a></button>
+		</div>
+	</div>
