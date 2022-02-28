27abff793b8067959ca1161a4294aa8d8a1906be
yaroslavekimov <yaroslavmur@mail.ru>
2022-02-28 20:53:15 +0300

    add coffeePage

--- php/views/coffee_view.php
+++ php/views/coffee_view.php
@@ -2,19 +2,20 @@
 	<div class="goodsPage Page">
 		<h1 class="goodsPageTitle Title">Товар</h1>
 		<div class="goodsPageWrapper Wrapper">
-			<?php 
-				for ($row_no = 0; $row_no <= $data->num_rows - 1; $row_no++) {
+			<?php for ($row_no = 0; $row_no <= $data->num_rows - 1; $row_no++) {
 					$data->data_seek($row_no);
 					$row = $data->fetch_assoc();
 					echo '
 			<div class="goodsPageList">
-					<img src="'.$row['src'].'" alt="coffee-shop" class="goodsPageImg">'.
-					'<div class="goodsPageItem">
-						<div class="goodsPageName">'.$row['product'].'</div>
-						<a href="/coffee-shop/goods/coffee/?id='.$row['id'].'">Подробнее</a>
-					</div>
-				</div>';
+				<div class="goodsPageItem">
+					<div class="goodsPageName">'.$row['product'].'</div>
+					<div class="goodsPageButton Button">Купить</div>
+				</div>
+				<div class="goodsPageText">'.$row['description'].'</div>
+			</div>';
 				}
 			?>
+		
 		</div>
 	</div>
+	
--- php/views/general_view.php
+++ php/views/general_view.php
@@ -1,8 +1,8 @@
 	
 	<div class="generalPage">
-		<div class="generalPageImg"><img src="products/canephora.png" alt="coffee shop"></div>
+		<div class="generalPageImg"><img src="prodImg/canephora.png" alt="coffee shop"></div>
 		<div class="generalPagePromo">
 			<h1 class="generalPageTitle"><span>Пора сделать</span> <br> &nbsp; Перерыв на Кофе!</h1>
-			<button class="generalPageButton"><a href="#">Shop now</a></button>
+			<div class="generalPageButton Button"><a href="/coffee-shop/goods">Shop now</a></div>
 		</div>
 	</div>
