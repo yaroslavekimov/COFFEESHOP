52a8d0a7960f95cd00cb97a31d0210ecd134de7c
yaroslavekimov <yaroslavmur@mail.ru>
2022-03-02 09:08:55 +0300

    add animation seeds and leaves

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     ' use strict ';
 
+// Slider products
+
     let slideIndex = 1;
 
     function showSlide(index) {
@@ -17,18 +19,54 @@ window.addEventListener('DOMContentLoaded', function() {
         slides.forEach((item) => item.style.display = 'none');
 
         slides[slideIndex - 1].style.display = 'flex';
+
+        document.querySelectorAll('.controlButton')[0].addEventListener('click', function() {
+            showSlide(--slideIndex);
+        })
+    
+        document.querySelectorAll('.controlButton')[1].addEventListener('click', function() {
+            showSlide(++slideIndex);
+        })
+
+        let timer = setInterval(() => document.querySelectorAll('.controlButton')[1].click(), 6000);
     } 
 
-    showSlide(slideIndex);
+    (function startSlider(index) {
+        if (document.querySelectorAll('#products').length !== 0) {
+            showSlide(index);
+        }
+    })(slideIndex);
 
-    document.querySelectorAll('.controlButton')[0].addEventListener('click', function() {
-        showSlide(--slideIndex);
-    })
+// Seeds show
 
-    document.querySelectorAll('.controlButton')[1].addEventListener('click', function() {
-        showSlide(++slideIndex);
-    })
+    function showSeeds() {
+        let seedList = document.getElementsByClassName('seed'), count = 300;
+        for ( let i = 0; i < seedList.length; i++) {
+            function animationTime() {
+                seedList[i].style.display = 'inline-block';
+            }
+            setTimeout(animationTime, count);
+            count = count+300;
+        }
+    };
 
-    let timer = setInterval(() => document.querySelectorAll('.controlButton')[1].click(), 6000);
+    (function startSeeds() {
+        if (document.getElementsByClassName('seed').length !== 0) {
+            showSeeds();
+        }
+    })();
+
+// Leaves show
+
+    function showLeaves() {
+        let leaves = document.getElementsByClassName('leavesImg');
+        leaves[0].style.display = 'inline-block';
+    }
+
+    (function startLeaves() {
+        if (document.getElementsByClassName('leaves').length !== 0) {
+            showLeaves();
+        }
+    })();
 
 });
