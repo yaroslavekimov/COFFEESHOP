8dde166b4b2c196e091f75fc4234f472a287c01f
yaroslavekimov <yaroslavmur@mail.ru>
2022-03-05 11:16:44 +0300

    Fix Link Favicon and Css

--- php/header.php
+++ php/header.php
@@ -4,8 +4,8 @@
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Coffee-shop</title>
-	<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
-	<link rel="stylesheet" href="/coffee-shop/css/style.css">
+	<link rel="shortcut icon" href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/img/favicon.ico" type="image/x-icon">
+	<link rel="stylesheet" href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/css/style.css">
 </head>
 <body>
     <header>
@@ -15,10 +15,10 @@
             </div>
             <div class="headerMenu">
                 <ul class="headerList">
-                    <li class="headerItem"><a href="/coffee-shop">ГЛАВНАЯ</a></li>
-                    <li class="headerItem"><a href="/coffee-shop/products">ТОВАРЫ</a></li>
-                    <li class="headerItem"><a href="/coffee-shop/about">О НАС</a></li>
-                    <li class="headerItem"><a href="/coffee-shop/contacts">КОНТАКТЫ</a></li>
+                    <li class="headerItem"><a href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/">ГЛАВНАЯ</a></li>
+                    <li class="headerItem"><a href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/products">ТОВАРЫ</a></li>
+                    <li class="headerItem"><a href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/about">О НАС</a></li>
+                    <li class="headerItem"><a href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/contacts">КОНТАКТЫ</a></li>
                 </ul>
             </div>
         </div>
