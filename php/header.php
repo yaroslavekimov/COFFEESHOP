f10d5815c7094f13f3cbdb52a76ff3ed7db42479
yaroslavekimov <yaroslavmur@mail.ru>
2022-03-05 17:41:07 +0300

    open graph

--- php/header.php
+++ php/header.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html lang="ru" prefix="og: http://ogp.me/ns#">
 <head>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Coffee-shop. Интернет магазин качественного, отборного кофе. У нас вы найдете кофе таких сортов как Арабика, Робуста, Либерика, Эксцельса, Стенофила. Ассортимент нашего кофейного магазина включает кофе из Италии, Австрии, Германии, Латвии и России." />
     <meta name="keywords" content="coffee, shop, магазин, отборного, кофе, арабика, робуста, либерика, эксцельса, стенофила, кофейный, напиток, сорт">
+    <meta property="og:title" content="Интернет магазин кофе" />
+    <meta property="og:description" content="Интернет магазин качественного, отборного кофе. У нас вы найдете кофе таких сортов как Арабика, Робуста, Либерика, Эксцельса, Стенофила. Ассортимент нашего кофейного магазина включает кофе из Италии, Австрии, Германии, Латвии и России." />
+    <meta property="og:site_name" content="Coffee-shop" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://coffeeshop.ru" />
+    <meta property="og:image" content="https://coffeeshop.ru/prodImg/arabica.png" />
 	<title>Coffee-shop</title>
 	<link rel="shortcut icon" href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/img/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" href="<?php echo $_SERVER["REQUEST_SCHEME"].'://'.$_SERVER["HTTP_HOST"].'/' ; ?>coffee-shop/css/style.css">
